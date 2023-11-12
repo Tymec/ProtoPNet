ce20f146987060780abe08581b59fb31fbd1d602
Tymec <tymek1rt@hotmail.com>
2023-11-12 01:22:07 +0100

    Add robots.txt file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,3 +3,4 @@ from pathlib import Path
 CWD = Path.cwd()
 STATIC_DIR = CWD / "static"
 FAVICON_PATH = STATIC_DIR / "favicon.ico"
+ROBOTS_PATH = STATIC_DIR / "robots.txt"
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
-from app import FAVICON_PATH, STATIC_DIR
+from app import FAVICON_PATH, ROBOTS_PATH, STATIC_DIR
 
 app = FastAPI()
 
@@ -12,6 +12,11 @@ app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     return FileResponse(FAVICON_PATH)
+
+
+@app.get("/robots.txt", include_in_schema=False)
+async def robots():
+    return FileResponse(ROBOTS_PATH)
 
 
 @app.get("/")
