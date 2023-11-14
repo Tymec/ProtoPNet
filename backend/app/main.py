8cb6971e9adf5f8f3a352aff5ef42a1c13319670
Krystian Jachna <krysti4nape@gmail.com>
2023-11-14 23:49:38 +0100

    Added functionality to return heatmaps, boxes, or both based on query parameter (default: both).

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,24 +1,39 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
-
-from app import *
-from tempfile import NamedTemporaryFile
+from enum import Enum
 from os import remove
+from typing import Optional
 from uuid import uuid4
 
-# ? importing like this due to the loadmodel() function in the 'inference' module
-import sys
-sys.path.append(str(CWD) + "/net")
-from inference import *
+from fastapi import FastAPI, File, HTTPException, Query, UploadFile
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from net.inference import (
+    CLASSIFICATIONS,
+    box_by_top_k_prototype,
+    heatmap_by_top_k_prototype,
+    load_model,
+    predict,
+    sanity_check,
+)
+
+from app import BOX_DIR, CWD, FAVICON_PATH, HEATMAP_DIR, INFO_PATH, MODEL_PATH, ROBOTS_PATH, STATIC_DIR
+
+
+class ReturnType(str, Enum):
+    heatmaps = "heatmaps"
+    boxes = "boxes"
+    both = "both"
+
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+app.mount(
+    "/static",
+    StaticFiles(directory=STATIC_DIR),
+    name="static",
+)
 
 # load machine
-model_path = Path("model/100push0.7413.pth")
-model = load_model(model_path, 0)
+model = load_model(MODEL_PATH, 0)
 
 # model check
 if not sanity_check(model, INFO_PATH):
@@ -41,48 +56,58 @@ async def root():
 
 
 @app.post("/predict")
-async def get_prediction(image: UploadFile=File(...)):
-
+async def get_prediction(
+    image: UploadFile = File(...),
+    return_type: Optional[ReturnType] = Query(
+        ReturnType.both, description="Types of item to return ['both'/'heatmaps'/'boxes']"
+    )
+):
     # Check file type
-    if image.content_type not in ["image/jpeg", "image/png"]:
-        raise HTTPException(status_code=400, detail="Only JPEG OR PNG images are allowed.")
+    if image.content_type not in [
+        "image/jpeg",
+        "image/png",
+    ]:
+        raise HTTPException(
+            status_code=400,
+            detail="Only JPEG OR PNG images are allowed.",
+        )
 
-    contents = await image.read()   
+    contents = await image.read()
 
     # Temprarily saving the input image
-    input_file_path = STATIC_DIR / str(uuid4()) 
+    input_file_path = STATIC_DIR / str(uuid4())
     with input_file_path.open(mode="wb") as f:
         f.write(contents)
-    
+
     proto = predict(model, input_file_path, 0)
     # Remove input image
     remove(input_file_path)
 
-    # Save heatmaps
     heatmap_urls = []
-    heatmaps = heatmap_by_top_k_prototype(proto.activation, proto.pattern, proto.img, 10)
 
-    for heatmap in heatmaps:
-        uuid = uuid4()
-        heatmap.save(STATIC_DIR / f"{uuid}.jpg")
-        heatmap_urls.append(f"/static/{uuid}.jpg")
+    # Save heatmaps
+    if return_type == ReturnType.heatmaps or return_type == ReturnType.both:
+        heatmaps = heatmap_by_top_k_prototype(proto.activation, proto.pattern, proto.img, 10)
 
-    
-    # Save boxes
+        for heatmap in heatmaps:
+            heatmap_url = HEATMAP_DIR / f"{uuid4()}.jpg"
+            heatmap.save(heatmap_url)  # todo: use database to save img
+            heatmap_urls.append(heatmap_url.relative_to(CWD))
+
     box_urls = []
-    boxes = box_by_top_k_prototype(proto.activation, proto.pattern, proto.img, 10)
 
-    for box in boxes:
-        uuid = uuid4()
-        box.save(STATIC_DIR / f"{uuid}.jpg")
-        box_urls.append(f"/static/{uuid}.jpg")
+    # Save boxes
+    if return_type == ReturnType.boxes or return_type == ReturnType.both:
+        boxes = box_by_top_k_prototype(proto.activation, proto.pattern, proto.img, 10)
 
+        for box in boxes:
+            box_url = BOX_DIR / f"{uuid4()}.jpg"
+            box.save(box_url)  # todo: use databse to save img
+            box_urls.append(box_url.relative_to(CWD))
 
     # Response
     return {
         "prediction": CLASSIFICATIONS[proto.prediction],
         "heatmap_urls": heatmap_urls,
-        "box_urls": box_urls
+        "box_urls": box_urls,
     }
-
-# todo: delete static/*.jpg from time to time
