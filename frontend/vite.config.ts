2ed2df31ec4d6de08fe04b68ad0f9d37ef16f1e2
Tymec <tymek1rt@hotmail.com>
2023-11-15 11:16:12 +0100

    Init vite react project

---
+++ frontend/vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  publicDir: 'public',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // css: true,
+    coverage: {
+      provider: 'istanbul',
+      reportsDirectory: './coverage',
+    },
+  },
+  build: {
+    manifest: true,
+  },
+})
