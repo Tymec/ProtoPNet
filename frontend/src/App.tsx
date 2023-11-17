d84b943a524de65d2fbf33c120da6f5383fc1a89
Tymec <tymek1rt@hotmail.com>
2023-11-17 15:17:43 +0100

    Prototype frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import ImageDropzone from './components/ImageDropzone';
 
@@ -7,33 +8,66 @@ enum ReturnType {
   BOXES = 'boxes',
 }
 
-function predict(file: File, k: number = 10, returnType: ReturnType = ReturnType.BOTH) {
-  console.log(file);
-
-  const url =
-    `${import.meta.env.VITE_API_URL}/predict` +
-    new URLSearchParams({
-      k: k.toString(),
-      return_type: returnType,
-    });
-
-  fetch(url, {
-    method: 'POST',
-    body: file,
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
-
 export default function App() {
+  const [prediction, setPrediction] = useState<string | undefined>(undefined);
+  const [heatmapImages, setHeatmapImages] = useState<string[]>([]);
+  const [boxImages, setBoxImages] = useState<string[]>([]);
+
+  function predict(file: File, k: number = 10, returnType: ReturnType = ReturnType.BOTH) {
+    const url =
+      `${import.meta.env.VITE_API_URL}/predict?` +
+      new URLSearchParams({
+        k: k.toString(),
+        return_type: returnType,
+      });
+  
+    const formData = new FormData();
+    formData.append('image', file);
+  
+    fetch(url, {
+      method: 'POST',
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPrediction(data.prediction);
+        setHeatmapImages(data.heatmap_urls);
+        setBoxImages(data.box_urls);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+ 
   return (
     <div className="bg-gray-700 min-h-screen m-0 p-0">
       <ImageDropzone onUpload={predict} />
+      {prediction && (
+        <div className="flex flex-col items-center justify-center">
+          <h2 className="text-2xl font-semibold text-gray-100">Prediction</h2>
+          <p className="text-gray-400">{prediction}</p>
+        </div>
+      )}
+      <div className="flex flex-col items-center justify-center">
+        {heatmapImages.length > 0 && heatmapImages.map((image, index) => (
+          <img
+            key={index}
+            className="rounded-lg"
+            src={`${import.meta.env.VITE_API_URL}/${image}`}
+            alt="Heatmap"
+          />
+        ))}
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        {boxImages.length > 0 && boxImages.map((image, index) => (
+          <img
+            key={index}
+            className="rounded-lg"
+            src={`${import.meta.env.VITE_API_URL}/${image}`}
+            alt="Box"
+          />
+        ))}
+      </div>
     </div>
   );
 }
