194da259cad152873f039cad8e9efc331cedfb09
Tymec <tymek1rt@hotmail.com>
2023-11-18 16:58:25 +0100

    Organize app init

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
 CWD = Path.cwd()
+
 STATIC_DIR = CWD / "static"
 MODEL_DIR = CWD / "model"
+BOX_DIR = STATIC_DIR / "boxmap"
+HEATMAP_DIR = STATIC_DIR / "heatmap"
 
 FAVICON_PATH = STATIC_DIR / "favicon.ico"
 ROBOTS_PATH = STATIC_DIR / "robots.txt"
 MODEL_PATH = MODEL_DIR / "100push0.7413.state.pth"
 MODEL_INFO_PATH = MODEL_DIR / "bb100.npy"
-BOX_DIR = STATIC_DIR / "boxmap"
-HEATMAP_DIR = STATIC_DIR / "heatmap"
