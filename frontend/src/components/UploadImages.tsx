edb06aa3fad85708b3ed4e737d056f095389a9a5
Krystian Jachna <krysti4nape@gmail.com>
2023-11-21 02:18:40 +0100

    Added darkening effect for other images on hover

--- frontend/src/components/UploadImages.tsx
+++ frontend/src/components/UploadImages.tsx
@@ -5,11 +5,12 @@ interface HeatmapProps {
   images: string[];
 }
 
-const UploadImages: React.FC<HeatmapProps> = ({ images}) => {
+const UploadImages: React.FC<HeatmapProps> = ({ images }) => {
   const [imagesLoaded, setImagesLoaded] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   useEffect(() => {
-    setImagesLoaded(false)
+    setImagesLoaded(false);
     if (images.length > 0) {
       const loadPromises = images.map((src) => {
         return new Promise((resolve, reject) => {
@@ -26,25 +27,34 @@ const UploadImages: React.FC<HeatmapProps> = ({ images}) => {
     }
   }, [images]);
 
+  const handleImageHover = (index: number) => {
+    setHoveredIndex(index);
+  };
+
+  const handleImageLeave = () => {
+    setHoveredIndex(null);
+  };
+
   return (
     <div className={`bg-gray-200 g-gray-200 dark:bg-gray-700 p-4 rounded-lg relative ${imagesLoaded ? '' : 'opacity-50'}`}>
-      {!imagesLoaded && <LoadingWheel/>}
+      {!imagesLoaded && <LoadingWheel />}
 
       {imagesLoaded && (
         <div className="flex flex-row items-center justify-start flex-wrap gap-4">
           {images.map((image, index) => (
             <img
               key={index}
-              className="rounded-lg hover:scale-150 transition-transform duration-200 hover:z-10"
+              className={`hover:z-10 rounded-lg hover:scale-150 transition-transform duration-200 ${hoveredIndex !== null && hoveredIndex !== index ? 'opacity-50' : ''}`}
               src={`${import.meta.env.VITE_API_URL}/${image}`}
               alt="Heatmap"
+              onMouseEnter={() => handleImageHover(index)}
+              onMouseLeave={handleImageLeave}
             />
           ))}
         </div>
       )}
     </div>
   );
-  
 };
 
 export default UploadImages;
