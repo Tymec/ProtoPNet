096ac68054e72f1ac64f149d63e54b49a2526eef
Tymec <tymek1rt@hotmail.com>
2023-11-23 16:46:22 +0100

    Make map into a rotating globe

--- frontend/src/components/HabitatMap.tsx
+++ frontend/src/components/HabitatMap.tsx
@@ -1,71 +1,81 @@
 import worldJson from '@/assets/world.geo.json';
-import { useRef } from 'react';
-import { ComposableMap, Geographies, Geography, Marker, ZoomableGroup } from 'react-simple-maps';
-import { Tooltip, TooltipRefProps } from 'react-tooltip';
+import { timer } from 'd3';
+import { useEffect, useState } from 'react';
+import { ComposableMap, Geographies, Geography, Graticule } from 'react-simple-maps';
+import { Tooltip } from 'react-tooltip';
 
 interface HabitatMapProps {
   countries: string[];
   mapWidth?: number;
   mapHeight?: number;
+  graticule?: boolean;
 }
 
 export default function HabitatMap({
   countries,
   mapWidth = 800,
   mapHeight = 600,
+  graticule = false,
 }: HabitatMapProps) {
-  const tooltipRef = useRef<TooltipRefProps>(null);
+  const [tooltipContent, setTooltipContent] = useState('');
+  const [yaw, setYaw] = useState(0);
+
+  useEffect(() => {
+    const t = timer(() => {
+      setYaw((y) => y + 0.2);
+    });
+
+    return () => t.stop();
+  }, []);
 
   return (
     <>
-      <Tooltip ref={tooltipRef} />
+      <Tooltip
+        id="map-tooltip"
+        place="bottom"
+        float={true}
+        noArrow={false}
+        content={tooltipContent}
+        className="text-xs"
+      />
       <ComposableMap
-        className="h-full w-full"
+        data-tooltip-id="map-tooltip"
+        className="h-full w-full cursor-crosshair outline-none"
+        projection="geoOrthographic"
         projectionConfig={{
-          scale: 140,
+          rotate: [-yaw, 0, 0],
+          scale: 280,
         }}
         width={mapWidth}
         height={mapHeight}
       >
-        <ZoomableGroup zoom={1} onMoveStart={() => tooltipRef.current?.close()}>
-          <Geographies geography={worldJson}>
-            {({ geographies }) =>
-              geographies.map((geo) => {
-                const { fips_10 } = geo.properties;
+        {graticule && <Graticule stroke="#999" clipPath="url(#rsm-sphere)" />}
+        {/* <ZoomableGroup zoom={1} center={[0, 0]} onMoveStart={() => setTooltipContent('')}> */}
+        <Geographies geography={worldJson}>
+          {({ geographies }) =>
+            geographies.map((geo) => {
+              const { fips_10 } = geo.properties;
 
-                return (
-                  <>
-                    <Geography
-                      key={geo.rsmKey}
-                      stroke="#FFF"
-                      strokeWidth={0.5}
-                      filter="drop-shadow(0px 0px 1px rgb(0 0 0 / 0.2))"
-                      geography={geo}
-                      fill={countries.includes(fips_10) ? '#F53' : 'var(--tw-gradient-to)'}
-                      className={`to-gray-400 outline-none hover:outline-none focus:outline-none active:outline-none dark:to-gray-300
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  stroke="#FFF"
+                  strokeWidth={0.5}
+                  filter="drop-shadow(1px 0px 2px rgb(0 0 0 / 0.5))"
+                  geography={geo}
+                  fill={countries.includes(fips_10) ? '#F53' : 'var(--tw-gradient-to)'}
+                  className={`to-gray-400 outline-none hover:outline-none focus:outline-none active:outline-none dark:to-gray-300
                     ${
                       countries.includes(fips_10) ? 'hover:fill-rose-400' : 'hover:fill-slate-100'
                     }`}
-                      onMouseEnter={() =>
-                        tooltipRef.current?.open({
-                          content: geo.properties.name,
-                          anchorSelect: `.geo-marker-${geo.rsmKey}`,
-                        })
-                      }
-                      onMouseLeave={() => tooltipRef.current?.close()}
-                    />
-                    <Marker
-                      key={geo.properties.name}
-                      coordinates={[geo.properties.label_x, geo.properties.label_y]}
-                    >
-                      <a className={`geo-marker-${geo.rsmKey}`} />
-                    </Marker>
-                  </>
-                );
-              })
-            }
-          </Geographies>
-        </ZoomableGroup>
+                  onMouseEnter={() => setTooltipContent(geo.properties.name)}
+                  onMouseLeave={() => setTooltipContent('')}
+                />
+              );
+            })
+          }
+        </Geographies>
+        {/* </ZoomableGroup> */}
       </ComposableMap>
     </>
   );
