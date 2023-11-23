b8edb37cf4fbfc60f0ca6751d54a5894fbfefda0
Tymec <tymek1rt@hotmail.com>
2023-11-23 11:58:05 +0100

    Add shadow, use tabler icon and add disabled state to upload button

--- frontend/src/components/UploadButton.tsx
+++ frontend/src/components/UploadButton.tsx
@@ -1,50 +1,30 @@
-import React, { useState } from 'react';
+import { IconCloudUpload } from '@tabler/icons-react';
+import { useState } from 'react';
 
 interface UploadButtonProps {
   onClick: () => void;
   isFileSelected: boolean;
 }
 
-const UploadButton: React.FC<UploadButtonProps> = ({ onClick, isFileSelected }) => {
+export default function UploadButton({ onClick, isFileSelected }: UploadButtonProps) {
   const [isClickedOnce, setIsClickedOnce] = useState(false);
-
-  const handleClick = () => {
-    if (isFileSelected || isClickedOnce) {
-      onClick();
-    } else {
-      alert('Please select a file before uploading.');
-      setIsClickedOnce(true);
-    }
-  };
 
   return (
     <button
-      className={`ml-2 bg-blue-500 w-30 text-white rounded-lg p-2 ${
-        isFileSelected
-          ? 'transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-blue-600 duration-200'
-          : 'cursor-not-allowed'
-      }`}
-      onClick={handleClick}
-      disabled={!isFileSelected && isClickedOnce}
+      className={`w-30 flex select-none flex-row items-center justify-center gap-2 rounded-lg bg-blue-500 p-2 text-white shadow-inner shadow-black transition-all delay-100 duration-100 ease-in-out disabled:scale-100 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:opacity-50 disabled:hover:scale-100 disabled:hover:cursor-not-allowed disabled:hover:bg-gray-500 disabled:hover:opacity-50
+      ${isFileSelected ? 'bg-blue-500 hover:scale-105 hover:bg-blue-600' : 'cursor-not-allowed'}`}
+      onClick={() => {
+        if (isFileSelected || isClickedOnce) {
+          onClick();
+        } else {
+          alert('Please select a file before uploading.');
+          setIsClickedOnce(true);
+        }
+      }}
+      disabled={!isFileSelected && !isClickedOnce}
     >
-      <span className="mr-2">Upload</span>
-      <svg
-        className="w-6 h-6 text-white dark:text-white inline-block"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 20 16"
-      >
-        <path
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-        />
-      </svg>
+      <span className="">Upload</span>
+      <IconCloudUpload className="text-white dark:text-white" size={28} />
     </button>
   );
-};
-
-export default UploadButton;
+}
