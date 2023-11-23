3f342973235e976071fb3debabc83483fc4058c3
Tymec <tymek1rt@hotmail.com>
2023-11-23 14:10:39 +0100

    Add habitat map tooltip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -80,102 +80,104 @@ export default function App() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col gap-4 bg-white p-8 dark:bg-slate-800">
-      <div className="flex flex-row flex-wrap items-stretch justify-center gap-4">
-        <div className={`flex-shrink flex-grow basis-1/6 ${loading ? 'animate-pulse' : ''}`}>
-          <ImageDropzone
-            onUpload={(file: File) => {
-              setConfidenceData({});
-              setHabitatData([]);
-              setHeatmapImages([]);
-              setBoxImages([]);
-              setSelectedFile(file);
-            }}
-          />
-          {loading && <LoadingWheel absolute />}
-        </div>
-        <div
-          className={`flex flex-shrink flex-grow basis-1/5 flex-col rounded-lg bg-gray-200 p-4 dark:bg-gray-700 ${
-            loading ? 'animate-pulse' : ''
-          } shadow-md shadow-black`}
-        >
-          {loading && <LoadingWheel absolute />}
-          <h2
-            className="text-2xl font-semibold text-gray-800 shadow-white dark:text-gray-100 dark:shadow-black"
-            style={{
-              textShadow: '0px 3px 2px var(--tw-shadow-color)',
-            }}
+    <div className="min-h-screen bg-white p-8 dark:bg-slate-800">
+      <div className="mx-auto flex w-3/4 flex-col gap-4">
+        <div className="flex flex-row flex-wrap items-stretch justify-center gap-4">
+          <div className={`flex-shrink flex-grow basis-1/6 ${loading ? 'animate-pulse' : ''}`}>
+            <ImageDropzone
+              onUpload={(file: File) => {
+                setConfidenceData({});
+                setHabitatData([]);
+                setHeatmapImages([]);
+                setBoxImages([]);
+                setSelectedFile(file);
+              }}
+            />
+            {loading && <LoadingWheel absolute />}
+          </div>
+          <div
+            className={`flex flex-shrink flex-grow basis-1/5 flex-col rounded-lg bg-gray-200 p-4 dark:bg-gray-700 ${
+              loading ? 'animate-pulse' : ''
+            } shadow-md shadow-black`}
           >
-            Predictions:
-          </h2>
-          <div className="flex flex-col gap-4">
-            {Object.entries(confidenceData).map(([label, confidence], index) => (
-              <div key={index}>
-                <div className="mb-1 flex justify-between">
-                  <span
-                    className={`text-black-700 text-base font-medium ${
-                      index === 0 ? getCoinfidanceColor(confidence) : 'dark:text-white'
-                    }`}
-                  >
-                    {label}
-                  </span>
-                  <span className="text-black-700 text-sm font-medium dark:text-white">
-                    {confidence < 0.0001 ? '<0.01' : (confidence * 100).toFixed(2)}%
-                  </span>
+            {loading && <LoadingWheel absolute />}
+            <h2
+              className="text-2xl font-semibold text-gray-800 shadow-white dark:text-gray-100 dark:shadow-black"
+              style={{
+                textShadow: '0px 3px 2px var(--tw-shadow-color)',
+              }}
+            >
+              Predictions:
+            </h2>
+            <div className="flex flex-col gap-4">
+              {Object.entries(confidenceData).map(([label, confidence], index) => (
+                <div key={index}>
+                  <div className="mb-1 flex justify-between">
+                    <span
+                      className={`text-black-700 text-base font-medium ${
+                        index === 0 ? getCoinfidanceColor(confidence) : 'dark:text-white'
+                      }`}
+                    >
+                      {label}
+                    </span>
+                    <span className="text-black-700 text-sm font-medium dark:text-white">
+                      {confidence < 0.0001 ? '<0.01' : (confidence * 100).toFixed(2)}%
+                    </span>
+                  </div>
+                  <div className="h-2.5 w-full rounded-full bg-gray-400 shadow-inner shadow-black dark:bg-gray-600">
+                    <div
+                      className="h-2.5 animate-slider rounded-full bg-blue-500 shadow-sm shadow-black ease-in-out"
+                      style={{ width: `${Math.round(confidence * 100)}%` }}
+                    />
+                  </div>
                 </div>
-                <div className="h-2.5 w-full rounded-full bg-gray-400 shadow-inner shadow-black dark:bg-gray-600">
-                  <div
-                    className="h-2.5 animate-slider rounded-full bg-blue-500 shadow-sm shadow-black ease-in-out"
-                    style={{ width: `${Math.round(confidence * 100)}%` }}
-                  />
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-auto flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md shadow-black dark:bg-gray-700">
+            <HabitatMap countries={habitatData} />
           </div>
         </div>
-        <div className="flex flex-auto flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md shadow-black dark:bg-gray-700">
-          <HabitatMap countries={habitatData} />
-        </div>
-      </div>
-      <div className="flex w-full flex-row flex-wrap items-center justify-center gap-16 rounded-lg bg-gray-200 p-4 shadow-md shadow-black dark:bg-gray-700">
-        <label htmlFor="k-option" className="text-gray-800 dark:text-gray-100">
-          K:
-          <input
-            id="k-option"
-            type="number"
-            min="1"
-            max="100"
-            placeholder="1-100"
-            value={optionK}
-            onChange={(e) => setOptionK(parseInt(e.target.value))}
-            className="ml-2 rounded-lg bg-gray-500 p-2 text-gray-200 shadow-inner shadow-black outline-none dark:bg-gray-800 dark:text-gray-100"
+        <div className="flex w-full flex-row flex-wrap items-center justify-center gap-16 rounded-lg bg-gray-200 p-4 shadow-md shadow-black dark:bg-gray-700">
+          <label htmlFor="k-option" className="text-gray-800 dark:text-gray-100">
+            K:
+            <input
+              id="k-option"
+              type="number"
+              min="1"
+              max="100"
+              placeholder="1-100"
+              value={optionK}
+              onChange={(e) => setOptionK(parseInt(e.target.value))}
+              className="ml-2 rounded-lg bg-gray-500 p-2 text-gray-200 shadow-inner shadow-black outline-none dark:bg-gray-800 dark:text-gray-100"
+            />
+          </label>
+          <label htmlFor="return-type" className="select-none text-gray-800 dark:text-gray-100">
+            Return:
+            <select
+              id="return-type"
+              value={optionReturnType}
+              onChange={(e) => setOptionReturnType(e.target.value as ReturnType)}
+              className="ml-2 rounded-lg bg-gray-500 p-2 text-gray-200 shadow-inner shadow-black outline-none dark:bg-gray-800 dark:text-gray-100"
+            >
+              <option value={ReturnType.BOTH}>Both</option>
+              <option value={ReturnType.HEATMAPS}>Heatmaps</option>
+              <option value={ReturnType.BOXES}>Boxes</option>
+              <option value={ReturnType.NONE}>None</option>
+            </select>
+          </label>
+          <ColorSchemeToggle />
+          <UploadButton
+            onClick={() => selectedFile && predict(selectedFile)}
+            isFileSelected={!!selectedFile}
           />
-        </label>
-        <label htmlFor="return-type" className="select-none text-gray-800 dark:text-gray-100">
-          Return:
-          <select
-            id="return-type"
-            value={optionReturnType}
-            onChange={(e) => setOptionReturnType(e.target.value as ReturnType)}
-            className="ml-2 rounded-lg bg-gray-500 p-2 text-gray-200 shadow-inner shadow-black outline-none dark:bg-gray-800 dark:text-gray-100"
-          >
-            <option value={ReturnType.BOTH}>Both</option>
-            <option value={ReturnType.HEATMAPS}>Heatmaps</option>
-            <option value={ReturnType.BOXES}>Boxes</option>
-            <option value={ReturnType.NONE}>None</option>
-          </select>
-        </label>
-        <ColorSchemeToggle />
-        <UploadButton
-          onClick={() => selectedFile && predict(selectedFile)}
-          isFileSelected={!!selectedFile}
-        />
-      </div>
+        </div>
 
-      {heatmapImages && heatmapImages.length > 0 && (
-        <ImageDrawer images={heatmapImages} overlay={boxImages} />
-      )}
-      {/* {boxImages && boxImages.length > 0 && <ImageDrawer images={boxImages} />} */}
+        {heatmapImages && heatmapImages.length > 0 && (
+          <ImageDrawer images={heatmapImages} overlay={boxImages} />
+        )}
+        {/* {boxImages && boxImages.length > 0 && <ImageDrawer images={boxImages} />} */}
+      </div>
     </div>
   );
 }
--- frontend/src/components/HabitatMap.tsx
+++ frontend/src/components/HabitatMap.tsx
@@ -1,41 +1,68 @@
-import worldJson from '@/assets/world2.geo.json';
-import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps';
+import worldJson from '@/assets/world.geo.json';
+import { useRef } from 'react';
+import { ComposableMap, Geographies, Geography, Marker, ZoomableGroup } from 'react-simple-maps';
+import { Tooltip, TooltipRefProps } from 'react-tooltip';
 
 interface HabitatMapProps {
   countries: string[];
+  width?: number;
 }
 
-export default function HabitatMap({ countries }: HabitatMapProps) {
-  return (
-    <ComposableMap
-      className="h-full w-full"
-      projectionConfig={{
-        scale: 140,
-      }}
-      width={800}
-    >
-      <ZoomableGroup zoom={1}>
-        <Geographies geography={worldJson}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const { fips_10 } = geo.properties;
+export default function HabitatMap({ countries, width = 800 }: HabitatMapProps) {
+  const tooltipRef = useRef<TooltipRefProps>(null);
 
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  stroke="#FFF"
-                  strokeWidth={0.5}
-                  filter="drop-shadow(0px 0px 1px rgb(0 0 0 / 0.2))"
-                  geography={geo}
-                  fill={countries.includes(fips_10) ? '#F53' : 'var(--tw-gradient-to)'}
-                  className={`to-gray-400 outline-none hover:outline-none focus:outline-none active:outline-none dark:to-gray-300
-                  ${countries.includes(fips_10) ? 'hover:fill-rose-400' : 'hover:fill-slate-100'}`}
-                />
-              );
-            })
-          }
-        </Geographies>
-      </ZoomableGroup>
-    </ComposableMap>
+  return (
+    <>
+      <Tooltip ref={tooltipRef} />
+      <ComposableMap
+        className="h-full w-full"
+        projectionConfig={{
+          scale: 140,
+        }}
+        width={width}
+      >
+        <ZoomableGroup zoom={1}>
+          <Geographies geography={worldJson}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const { fips_10 } = geo.properties;
+
+                return (
+                  <>
+                    <Geography
+                      key={geo.rsmKey}
+                      stroke="#FFF"
+                      strokeWidth={0.5}
+                      filter="drop-shadow(0px 0px 1px rgb(0 0 0 / 0.2))"
+                      geography={geo}
+                      fill={countries.includes(fips_10) ? '#F53' : 'var(--tw-gradient-to)'}
+                      className={`to-gray-400 outline-none hover:outline-none focus:outline-none active:outline-none dark:to-gray-300
+                    ${
+                      countries.includes(fips_10) ? 'hover:fill-rose-400' : 'hover:fill-slate-100'
+                    }`}
+                      onMouseEnter={() => {
+                        tooltipRef.current?.open({
+                          content: geo.properties.name,
+                          anchorSelect: `.geo-marker-${geo.rsmKey}`,
+                        });
+                      }}
+                      onMouseLeave={() => {
+                        tooltipRef.current?.close();
+                      }}
+                    />
+                    <Marker
+                      key={geo.properties.name}
+                      coordinates={[geo.properties.label_x, geo.properties.label_y]}
+                    >
+                      <a className={`geo-marker-${geo.rsmKey}`} />
+                    </Marker>
+                  </>
+                );
+              })
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </>
   );
 }
