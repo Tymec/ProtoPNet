af76c893703d8b98f5a9e6a35c4dc9e960f28acb
Tymec <tymek1rt@hotmail.com>
2023-11-27 01:34:26 +0100

    Update analytics injection in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,16 @@
 import App from '@/App.tsx';
 import { ColorSchemeProvider } from '@/contexts/ColorScheme';
 import '@/index.css';
-import { Analytics } from '@vercel/analytics/react';
+import { inject } from '@vercel/analytics';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ColorSchemeProvider>
       <App />
-      <Analytics />
     </ColorSchemeProvider>
   </React.StrictMode>
 );
