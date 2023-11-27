e10f14b503082018390537cf6872b5c8c5973377
Tymec <tymek1rt@hotmail.com>
2023-11-27 01:30:46 +0100

    Add @vercel/analytics package

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import App from '@/App.tsx';
 import { ColorSchemeProvider } from '@/contexts/ColorScheme';
 import '@/index.css';
+import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -8,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ColorSchemeProvider>
       <App />
+      <Analytics />
     </ColorSchemeProvider>
   </React.StrictMode>
 );
