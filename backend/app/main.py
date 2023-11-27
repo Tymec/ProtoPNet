976b19a009a0132d4f22d1ebd05e19dca1049672
Tymec <tymek1rt@hotmail.com>
2023-11-28 00:32:18 +0100

    Add wildcard CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -46,7 +46,7 @@ app.mount(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
