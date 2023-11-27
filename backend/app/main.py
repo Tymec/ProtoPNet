1157d30cc58994e233ec17c3b403523387261f03
Tymec <tymek1rt@hotmail.com>
2023-11-27 23:19:27 +0100

    Set CORS to wildcard

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,11 +38,7 @@ app.mount(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://proto-p-net.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
