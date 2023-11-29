542eb6aa75e4f1993a08d807d01855e756652c24
Krystian Jachna <krysti4nape@gmail.com>
2023-11-29 15:04:10 +0100

    display predictions carousel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,19 +9,12 @@ import {
 import { IconMap, IconMoon, IconSun, IconWorld } from '@tabler/icons-react';
 import { useContext, useEffect, useState } from 'react';
 import { ColorSchemeContext } from './contexts/ColorScheme';
-
-enum ReturnType {
-  NONE = 'none',
-  BOTH = 'both',
-  HEATMAPS = 'heatmaps',
-  BOXES = 'boxes',
-}
+import PredictionsCarousel from './components/PredictionsCarousel';
 
 export default function App() {
   const { colorScheme, setColorScheme } = useContext(ColorSchemeContext);
 
   const [optionK, setOptionK] = useState<number>(10);
-  const [optionReturnType, setOptionReturnType] = useState<ReturnType>(ReturnType.BOTH);
   const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
   const [loading, setLoading] = useState(false);
 
@@ -45,16 +38,6 @@ export default function App() {
       .catch((err) => console.error(err));
   }, []);
 
-  const getCoinfidanceColor = (confidence: number) => {
-    if (confidence > 0.8) {
-      return 'text-green-500';
-    } else if (confidence >= 0.2) {
-      return 'text-yellow-500';
-    } else {
-      return 'text-red-500';
-    }
-  };
-
   const predict = (file: File) => {
     let k = optionK;
     setLoading(true);
@@ -69,7 +52,6 @@ export default function App() {
     const formData = new FormData();
 
     formData.append('image', file);
-    formData.append('return_type', optionReturnType);
     formData.append('k', k.toString());
 
     fetch(url, {
@@ -95,11 +77,18 @@ export default function App() {
       .finally(() => setLoading(false));
   };
 
+  const updateHabitatData = (bird: string) => {
+    console.log(bird)
+    setHabitatData(
+      bird in habitats ? habitats[bird as keyof typeof habitats] : []
+    );
+  }
+
   return (
     <div className="min-h-screen bg-white p-8 dark:bg-slate-800">
       <div className="mx-auto flex w-3/4 flex-col gap-4">
         <div className="flex flex-row flex-wrap items-stretch justify-center gap-4">
-          <div className={`flex-shrink flex-grow basis-1/6 ${loading ? 'animate-pulse' : ''}`}>
+          <div className={`flex-shrink flex-grow basis-1/7 ${loading ? 'animate-pulse' : ''}`}>
             <ImageDropzone
               onUpload={(file: File) => {
                 setConfidenceData({});
@@ -111,45 +100,7 @@ export default function App() {
             />
             {loading && <LoadingWheel absolute />}
           </div>
-          <div
-            className={`flex flex-shrink flex-grow basis-1/5 flex-col rounded-lg bg-gray-200 p-4 dark:bg-gray-700 ${
-              loading ? 'animate-pulse' : ''
-            } shadow-md shadow-black`}
-          >
-            {loading && <LoadingWheel absolute />}
-            <h2
-              className="text-2xl font-semibold text-gray-800 shadow-white dark:text-gray-100 dark:shadow-black"
-              style={{
-                textShadow: '0px 3px 2px var(--tw-shadow-color)',
-              }}
-            >
-              Predictions:
-            </h2>
-            <div className="flex flex-col gap-4">
-              {Object.entries(confidenceData).map(([label, confidence], index) => (
-                <div key={index}>
-                  <div className="mb-1 flex justify-between">
-                    <span
-                      className={`text-black-700 text-base font-medium ${
-                        index === 0 ? getCoinfidanceColor(confidence) : 'dark:text-white'
-                      }`}
-                    >
-                      {label}
-                    </span>
-                    <span className="text-black-700 text-sm font-medium dark:text-white">
-                      {confidence < 0.0001 ? '<0.01' : (confidence * 100).toFixed(2)}%
-                    </span>
-                  </div>
-                  <div className="h-2.5 w-full rounded-full bg-gray-400 shadow-inner shadow-black dark:bg-gray-600">
-                    <div
-                      className="h-2.5 animate-slider rounded-full bg-blue-500 shadow-sm shadow-black ease-in-out"
-                      style={{ width: `${Math.round(confidence * 100)}%` }}
-                    />
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
+          <PredictionsCarousel confidenceData={confidenceData} loading={loading} onUpdateBird={updateHabitatData}/>
           <div className="flex flex-auto flex-col items-center justify-center rounded-lg bg-gray-200 shadow-md shadow-black dark:bg-gray-700">
             <HabitatMap countries={habitatData} globe={globeMap} />
           </div>
@@ -168,20 +119,7 @@ export default function App() {
               className="ml-2 rounded-lg bg-gray-500 p-2 text-gray-200 shadow-inner shadow-black outline-none dark:bg-gray-800 dark:text-gray-100"
             />
           </label>
-          <label htmlFor="return-type" className="select-none text-gray-800 dark:text-gray-100">
-            Return:
-            <select
-              id="return-type"
-              value={optionReturnType}
-              onChange={(e) => setOptionReturnType(e.target.value as ReturnType)}
-              className="ml-2 rounded-lg bg-gray-500 p-2 text-gray-200 shadow-inner shadow-black outline-none dark:bg-gray-800 dark:text-gray-100"
-            >
-              <option value={ReturnType.BOTH}>Both</option>
-              <option value={ReturnType.HEATMAPS}>Heatmaps</option>
-              <option value={ReturnType.BOXES}>Boxes</option>
-              <option value={ReturnType.NONE}>None</option>
-            </select>
-          </label>
+          
           <IconToggle
             IconOn={<IconWorld />}
             IconOff={<IconMap />}
