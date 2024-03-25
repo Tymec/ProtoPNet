4c6380168b81826e2249387913a796c868d8e6d0
Tymec <tymek1rt@hotmail.com>
2024-03-25 13:26:19 +0100

    Add SEO stuff

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
   readonly VITE_API_URL: string;
   readonly VITE_HABITAT_GEO_URL: string;
   readonly VITE_HABITAT_DATA_URL: string;
