3965d26e17c6128224ae55a53232b8f12e69689b
krystian <krysti4nape@gmail.com>
2024-05-19 07:16:45 +0200

    added fun to get history by user_id

--- backend/app/firebase.py
+++ backend/app/firebase.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from app import FIREBASE_SA_PATH, FIREBASE_COLLECTION 
 from datetime import datetime
+from typing import List
 
 class FirebaseManager:
     def __init__(self) -> None:
@@ -19,7 +20,7 @@ class FirebaseManager:
             "boxmaps": boxmaps,
             "flagged": flagged,
             "user_id": user_id,
-            "created_at": current_datetime,
+            "timestamp": current_datetime,
         })
         doc_id = doc_ref[1].id 
         return doc_id
@@ -30,3 +31,18 @@ class FirebaseManager:
             "flagged": flagged
         })
 
+    def get_user_history(self, user_id: str) -> List[dict]:
+        docs = self.collection.where("user_id", "==", user_id).stream()
+        
+        user_history = []
+        for doc in docs:
+            data = doc.to_dict()
+            user_history.append({
+                "image": data["image"],
+                "prediction": data["prediction"],
+                "heatmaps": data["heatmaps"],
+                "flagged": data["flagged"],
+                "timestamp": data["timestamp"],
+            })
+        
+        return user_history
